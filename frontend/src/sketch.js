ce84c2e6dd76e49f783684686b665e3c31e4f0b2
annxiesun <anniesun@Annies-MacBook-Pro-2.local>
2024-03-06 14:19:01 -0500

    clean up

--- frontend/src/sketch.js
+++ frontend/src/sketch.js
@@ -12,9 +12,10 @@ const FRAME_NUM = 78;
 export const mySketch = (p) => {
   // the model
   p.poseNet;
+
   // latest model predictions
   p.predictions = [];
-  // video capture
+
   p.video;
 
   p.eyeVideoImages = [];
@@ -22,37 +23,30 @@ export const mySketch = (p) => {
   p.windowSections = 10;
 
   p.currVideoTime = 0;
-  // p.desiredVideoTime = 3;
-  p.loaded = false;
 
-  p.prevMouseX = 0;
-
-  p.lock = false;
-
-  p.preload = () => {};
+  p.preload = () => {
+    for (i = 1; i <= FRAME_NUM; i++) {
+      let fn = 'eye_video/' + "ezgif-frame-" + String(i).padStart(3, '0') + ".jpg"
+      p.eyeVideoImages.push(p.loadImage(fn))
+    }
+  };
 
   p.setup = () => {
     p.createCanvas(window.innerWidth, window.innerHeight);
     p.noLoop();
     p.windowSectionWidth = p.width / p.windowSections;
 
-    // p.video = p.createCapture(p.VIDEO);
-    // p.video.size(p.width, p.height);
-
+    p.video = p.createCapture(p.VIDEO);
+    p.video.size(p.width, p.height);
+    p.video.hide();
   
-    // // initialize the model
-    // p.posenet = ml5.poseNet(video, p.modelReady);
-    // p.posenet.on("pose", (results) => {
-    //   predictions = results;
-    // });
+    // initialize the model
+    p.posenet = ml5.poseNet(p.video, p.modelReady);
+    p.posenet.on("pose", (results) => {
+      p.predictions = results;
+    });
 
-    // Hide the video element, and just show the canvas
-    // p.video.hide();
-  
-    for (i = 1; i <= FRAME_NUM; i++) {
-      let fn = 'eye_video/' + "ezgif-frame-" + String(i).padStart(3, '0') + ".jpg"
-      p.eyeVideoImages.push(p.loadImage(fn))
-    }
+    //Hide the video element, and just show the canvas
 
 
     const moveAround = setInterval(() => {
@@ -96,92 +90,37 @@ export const mySketch = (p) => {
   };
 
   p.draw = () => {
-    // console.log("draw")
-    // if (!p.loaded) return;
     p.background("#0000aa");
     // draw the vdieo
     p.image(p.eyeVideoImages[p.currVideoTime],0,0, p.width, p.height)
 
-    // predictions.forEach((pred) => {
-    //   const landmarks = pred.pose.keypoints;
-  
-    //   landmarks.forEach((k) => {
-    //     const x = k.position.x;
-    //     const y = k.position.y;
-  
-    //     stroke(128);
-    //     strokeWeight(1);
-    //     fill(255);
-    //     ellipse(x, y, 10, 10);
-    //   });
-    // });
-    // p.image(video, 0, 0, p.width, p.height);
-
-    // different visualizations
-    // p.drawBoundingBoxes();
-
-    // if (p.predictions.length > 0) {
-    //   drawAnnotation(p.predictions[0], "silhouette")
-    // }
+    p.predictions.forEach((prediction) => {
+      let a = prediction.pose["nose"];
+      const x = a.x;
+      const y = a.y;
+      p.noStroke();
+      p.fill('red');
+      p.ellipse(x, y, 12);
+    })
 
     // debug info
     // p.drawFps();
   };
 
   // draw the bounding box for first face
-  p.drawBoundingBoxes = () => {
-    let c = "#ff0000";
-    for (let predict of p.predictions) {
-      const bb = predict.boundingBox;
-      // get bb coordinates
-      const x = bb.topLeft[0][0];
-      const y = bb.topLeft[0][1];
-      const w = bb.bottomRight[0][0] - x;
-      const h = bb.bottomRight[0][1] - y;
-
-      // draw the bounding box
-      p.stroke(c);
-      p.strokeWeight(2);
-      p.noFill();
-      p.rect(x, y, w, h);
-      // draw the confidence
-      p.noStroke();
-      p.fill(c);
-      p.textAlign(p.LEFT, p.BOTTOM);
-      p.textSize(20.0);
-      p.text(predict.faceInViewConfidence.toFixed(2), x, y - 10);
-    }
-  };
-
-  let upperEyeLeft = [];
-  let upperEyeRight = [];
-
-  p.drawAllAnnotations = () => {
-    for (let prediction of p.predictions) {
-      let keyNum = Object.keys(p.annotations).length;
-      let i = 0;
-      for (let n in prediction.annotations) {
-        // make a rainbow
-        let hue = map(i++, 0, keyNum, 0, 360);
-        let c = color(`hsb(${hue}, 100%, 100%)`);
-        // draw the annotation
-        drawAnnotation(p, n, c);
-      }
-    }
-  };
   p.mousePressed = () => {
   }
 
   p.mouseMoved = () => {
-    const diff = p.prevMouseX - p.mouseX
-    p.currVideoTime = Math.floor(p.mouseX/p.width * FRAME_NUM)
-    p.currVideoTime = Math.min(p.currVideoTime, FRAME_NUM - 1)
-    p.currVideoTime = Math.max(p.currVideoTime, 0)
+    // const diff = p.prevMouseX - p.mouseX
+    // p.currVideoTime = Math.floor(p.mouseX/p.width * FRAME_NUM)
+    // p.currVideoTime = Math.min(p.currVideoTime, FRAME_NUM - 1)
+    // p.currVideoTime = Math.max(p.currVideoTime, 0)
 
-    p.draw()
-    p.prevMouseX = p.mouseX
-    // console.log(diff)
-    p.lock = false;
+    // p.draw()
+    // p.prevMouseX = p.mouseX
+    // // console.log(diff)
+    // p.lock = false;
   };
 
   p.windowResized = () => {};
