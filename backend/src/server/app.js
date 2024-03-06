6e66ee5930a4d6a803d54831f44521e505f0c174
annxiesun <anniesun@Annies-MacBook-Pro-2.local>
2024-03-06 15:20:20 -0500

    backend

--- backend/src/server/app.js
+++ backend/src/server/app.js
@@ -3,13 +3,13 @@
 const express = require('express');
 const cors = require('cors');
 const routerMiddleware = require('./routes/REST_routes');
+const serverless = require("serverless-http");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('../public'));
 
 routerMiddleware(app);
 
-module.exports = app;
+module.exports = serverless(app);
