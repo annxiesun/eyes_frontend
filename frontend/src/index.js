a275fc9301952d6564312585a801dc5febf36001
annxiesun <anniesun@Annies-MacBook-Pro-2.local>
2024-03-06 14:10:20 -0500

    eye working

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,8 @@ import './index.css'
 
 // Initialize p5.js
 // p5 requires two arguments: new p5(sketch function, target DOM element)
+
+//uncomment soon
 new p5(mySketch, document.getElementById('sketch'))
 
 // // Example: update the DOM
@@ -21,3 +23,68 @@ if (process.env.NODE_ENV !== 'production') {
     location.reload(),
   )
 }
+
+// const form = document.querySelector('#form');
+// const input = document.querySelector('#chat-message');
+// const button = document.querySelector('#chat-button');
+// const socketCount = document.querySelector('#socket-count');
+// const clientId = document.querySelector('#socket-id');
+// const homeLink = document.querySelector('#home-section-link');
+// const docsLink = document.querySelector('#documentation-link');
+// const homeSection = document.querySelector('#home-section');
+// const docsSection = document.querySelector('#documentation');
+
+// const socket = io('http://localhost:5000');
+// socket.emit('/root/new_socket_connected');
+
+// // Socket events
+// // Whenever the server emits '/root/welcome' event, update website
+// socket.on('/root/welcome', welcomeUser);
+// // Whenever the server emits '/root/update_socket_count' event, updates number of sockets connected
+// socket.on('root/update_socket_count', updateSocketCount);
+// // Whenever the server emits '/root/update_chat' event, add message to the chat
+// socket.on('/root/update_chat', addMessage);
+
+// // Socket event handlers
+// function addMessage (data) {
+//   const { message, sender } =  data;
+//   const text = document.createTextNode(message);
+//   const listItem = document.createElement('li');
+//   sender === 'user'
+//     ? listItem.classList.add('list-item', 'right')
+//     : listItem.classList.add('list-item', 'left');
+//   listItem.style.backgroundColor = `rgba(${randomColor()},${randomColor()},${randomColor()}, .5)`;
+//   const chat = document.getElementById('chat-list');
+//   listItem.appendChild(text);
+//   sender === 'user'
+//     ? setTimeout(() => chat.appendChild(listItem), 200)
+//     : setTimeout(() => chat.appendChild(listItem), 1000);
+// }
+
+// function welcomeUser (data) {
+//   const { message, sender, id } = data;
+//   addMessage({ message, sender });
+//   clientId.innerHTML = id;
+// }
+
+// function updateSocketCount (data) {
+//   const { clientCount } = data;
+//   socketCount.innerHTML = clientCount;
+// }
+
+// // Sends a chat message to the server
+// function sendMessage (e) {
+//   e.preventDefault();
+//   const value = input.value;
+//   addMessage({message: value, sender: 'user'});
+//   input.value = '';
+//   socket.emit('/root/new_message', value);
+// }
+
+// function randomColor () {
+//   return Math.floor(Math.random()*255);
+// }
+
+// // Reactive elements > Event listeners
+// form.addEventListener('submit', sendMessage);
+// button.addEventListener('click', sendMessage);
--- frontend/src/sketch.js
+++ frontend/src/sketch.js
@@ -1,71 +1,135 @@
 import { createParamGui } from "./utilities";
 // import PREDICTIONS from "./predictions.json";
 
+
 export const OPTIONS = {
   agentNum: 20,
-  phase: 0
-}
-
+  phase: 0,
+};
+const FRAME_NUM = 78;
 
 // Exporting a p.called 'mySketch'
 export const mySketch = (p) => {
   // the model
-  p.facemesh;
+  p.poseNet;
   // latest model predictions
-  p.predictions = []
+  p.predictions = [];
   // video capture
   p.video;
-  
-  p.preload = () => {}
-  
+
+  p.eyeVideoImages = [];
+
+  p.windowSections = 10;
+
+  p.currVideoTime = 0;
+  // p.desiredVideoTime = 3;
+  p.loaded = false;
+
+  p.prevMouseX = 0;
+
+  p.lock = false;
+
+  p.preload = () => {};
+
   p.setup = () => {
     p.createCanvas(window.innerWidth, window.innerHeight);
+    p.noLoop();
+    p.windowSectionWidth = p.width / p.windowSections;
+
+    // p.video = p.createCapture(p.VIDEO);
+    // p.video.size(p.width, p.height);
+
   
-    video = p.createCapture(p.VIDEO);
-    video.size(p.width, p.height);
-  
-  
-    // initialize the model
-    const options = {
-      // flipHorizontal: true, // seems to be a bug?
-      maxFaces: 1,
-      detectionConfidence: 0.5,
-    };
-    p.facemesh = ml5.facemesh(video, options, p.modelReady);
-  
-    // This sets up an event that fills the global variable "predictions"
-    // with an array every time new predictions are made
-    p.facemesh.on("predict", (results) => {
-      p.predictions = results;
-    });
-  
+    // // initialize the model
+    // p.posenet = ml5.poseNet(video, p.modelReady);
+    // p.posenet.on("pose", (results) => {
+    //   predictions = results;
+    // });
+
     // Hide the video element, and just show the canvas
-    video.hide();
-  }
+    // p.video.hide();
   
-  p.modelReady= () => {
+    for (i = 1; i <= FRAME_NUM; i++) {
+      let fn = 'eye_video/' + "ezgif-frame-" + String(i).padStart(3, '0') + ".jpg"
+      p.eyeVideoImages.push(p.loadImage(fn))
+    }
+
+
+    const moveAround = setInterval(() => {
+
+      p.currVideoTime += 1
+
+      p.currVideoTime = Math.min(p.currVideoTime, FRAME_NUM - 1)
+      p.currVideoTime = Math.max(p.currVideoTime, 0)
+      p.draw()
+
+      setTimeout(() => {
+        p.currVideoTime += 1
+
+        p.currVideoTime = Math.min(p.currVideoTime, FRAME_NUM - 1)
+        p.currVideoTime = Math.max(p.currVideoTime, 0)
+        p.draw()
+      }, 100)
+
+      setTimeout(() => {
+        p.currVideoTime -= 1
+
+        p.currVideoTime = Math.min(p.currVideoTime, FRAME_NUM - 1)
+        p.currVideoTime = Math.max(p.currVideoTime, 0)
+        p.draw()
+      }, 200)
+
+
+      setTimeout(() => {
+        p.currVideoTime -= 1
+
+        p.currVideoTime = Math.min(p.currVideoTime, FRAME_NUM - 1)
+        p.currVideoTime = Math.max(p.currVideoTime, 0)
+        p.draw()
+      }, 300)
+    
+    }, 400)
+
+  };
+  p.modelReady = () => {
     console.log("Model ready!");
-  }
-  
-  p.draw= () => {
+  };
+
+  p.draw = () => {
+    // console.log("draw")
+    // if (!p.loaded) return;
     p.background("#0000aa");
-  
     // draw the vdieo
+    p.image(p.eyeVideoImages[p.currVideoTime],0,0, p.width, p.height)
+
+    // predictions.forEach((pred) => {
+    //   const landmarks = pred.pose.keypoints;
+  
+    //   landmarks.forEach((k) => {
+    //     const x = k.position.x;
+    //     const y = k.position.y;
+  
+    //     stroke(128);
+    //     strokeWeight(1);
+    //     fill(255);
+    //     ellipse(x, y, 10, 10);
+    //   });
+    // });
     // p.image(video, 0, 0, p.width, p.height);
-  
+
     // different visualizations
-    p.drawBoundingBoxes();
-  
+    // p.drawBoundingBoxes();
+
     // if (p.predictions.length > 0) {
     //   drawAnnotation(p.predictions[0], "silhouette")
     // }
-  
+
     // debug info
-    p.drawFps();
-  }
-  
+    // p.drawFps();
+  };
+
   // draw the bounding box for first face
-  p.drawBoundingBoxes= () => {
+  p.drawBoundingBoxes = () => {
     let c = "#ff0000";
     for (let predict of p.predictions) {
       const bb = predict.boundingBox;
@@ -74,7 +138,7 @@ export const mySketch = (p) => {
       const y = bb.topLeft[0][1];
       const w = bb.bottomRight[0][0] - x;
       const h = bb.bottomRight[0][1] - y;
-  
+
       // draw the bounding box
       p.stroke(c);
       p.strokeWeight(2);
@@ -87,17 +151,15 @@ export const mySketch = (p) => {
       p.textSize(20.0);
       p.text(predict.faceInViewConfidence.toFixed(2), x, y - 10);
     }
-  }
-  
+  };
+
   let upperEyeLeft = [];
   let upperEyeRight = [];
-  
-  
-  p.drawAllAnnotations= () => {
+
+  p.drawAllAnnotations = () => {
     for (let prediction of p.predictions) {
       let keyNum = Object.keys(p.annotations).length;
       let i = 0;
-      drawingContext.filter = 'blur(5px)';
       for (let n in prediction.annotations) {
         // make a rainbow
         let hue = map(i++, 0, keyNum, 0, 360);
@@ -106,24 +168,27 @@ export const mySketch = (p) => {
         drawAnnotation(p, n, c);
       }
     }
-    drawingContext.filter = 'none';
+  };
+  p.mousePressed = () => {
   }
-  
-  p.keyPressed= () => {
-    if (key == "?") {
-      if (predictions) print(JSON.stringify(predictions, null, 2));
-    } else if (key === "a") {
-      drawAnnotations();
-    }
-  }
-  
-  p.mousePressed= () => {}
-  
-  p.windowResized= () => {}
-  
+
+  p.mouseMoved = () => {
+    const diff = p.prevMouseX - p.mouseX
+    p.currVideoTime = Math.floor(p.mouseX/p.width * FRAME_NUM)
+    p.currVideoTime = Math.min(p.currVideoTime, FRAME_NUM - 1)
+    p.currVideoTime = Math.max(p.currVideoTime, 0)
+
+    p.draw()
+    p.prevMouseX = p.mouseX
+    // console.log(diff)
+    p.lock = false;
+  };
+
+  p.windowResized = () => {};
+
   p.fps = 0;
-  
-  p.drawFps= () => {
+
+  p.drawFps = () => {
     let a = 0.01;
     p.fps = a * p.frameRate() + (1 - a) * p.fps;
     p.stroke(255);
@@ -132,6 +197,5 @@ export const mySketch = (p) => {
     p.textAlign(p.LEFT, p.TOP);
     p.textSize(20.0);
     p.text(p.fps.toFixed(1), 10, 10);
-  }
-  
-}
+  };
+};
